14dc1960483be7b8030997c7bf361774e49c5e9b
liangwuqiang <l_wuqiang@126.com>
2017-07-17 08:19:43 +0800

    a

--- src/main.kt
+++ src/main.kt
@@ -1,29 +1,12 @@
-interface A {
-    fun foo() { println("A foo") }
-    fun bar()
-}
-interface B {
-    fun foo() { println("B foo") }
-    fun bar() { println("B bar") }
-}
-class C : A {
-    override fun bar() { println("C bar") }
-}
-class D : A, B {
-    override fun foo() {
-        super<A>.foo()
-        super<B>.foo()
-    }
-    override fun bar() {
-        super<B>.bar()
-    }
-}
 
 fun main(args: Array<String>) {
-    var c = C()
-    c.bar()
-    c.foo()
-    var d = D()
-    d.foo()
-    d.bar()
+    var language = if (args.size == 0) "En" else args[0]
+    println( when (language) {
+        "En" -> "Hello"
+        "Cn" -> "你好"
+        "Fr" -> "Calut"
+        "It" -> "dfs"
+        else -> "Sorry, I can't greet you in $language yet!"
+    })
+
 }
